83021d2404ca9ebe74af466be5ef2d32e716c628
Ben Hamment <fisix1@gmail.com>
2016-10-07 12:06:09 +0100

    Links up nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
     root 'static_pages#index'
 
     get '/left-sidebar/', to: 'static_pages#left_sidebar'
+    get '/right-sidebar/', to: 'static_pages#right_sidebar'
+    get '/no-sidebar/', to: 'static_pages#no_sidebar'
 end
